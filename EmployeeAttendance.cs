cd0a66afcf0958ededc3b2fad22ad0cf919f165b
murgendrag <murgendra67@gmail.com>
2022-03-06 23:42:28 +0530

    Random function used

---
+++ EmployeeAttendance.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day8_PracticeProblems
+{
+    public class EmployeeAttendance
+    {
+        
+        public void Employeeattcheck()
+        {
+            
+            int Emplopee_Present = 1;
+            Random random = new Random();
+            int number = random.Next(2);
+            if (number == Emplopee_Present)
+            {
+                Console.WriteLine("Employee is present");
+
+            }
+            else
+            {
+                Console.WriteLine("Employee is absent");
+            }
+        }
+    }
+    
+}
+
