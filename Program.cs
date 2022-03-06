4fc861f707d4a87880328f2e48b3d0264adc707c
murgendrag <murgendra67@gmail.com>
2022-03-06 13:57:53 +0530

    object created

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿
+namespace Day8_PracticeProblems
+{
+     class Program
+    {
+         public static void Main(string[] args)
+        {
+            EmployeeAttendance Employee = new EmployeeAttendance();
+            Employee.Employeeattcheck();
+        }
+    }
+}
