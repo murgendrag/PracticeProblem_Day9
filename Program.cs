5a46d3fa5af6117ac5f7874ee09748ec0746fd95
murgendrag <murgendra67@gmail.com>
2022-03-07 16:26:34 +0530

    object created

--- Program.cs
+++ Program.cs
@@ -4,9 +4,13 @@ namespace Day8_PracticeProblems
      class Program
     {
          public static void Main(string[] args)
+        //{
+        //    EmployeeAttendance Employee1 = new EmployeeAttendance();
+        //    Employee.Employeeattcheck();
+        //}
         {
-            EmployeeAttendance Employee = new EmployeeAttendance();
-            Employee.Employeeattcheck();
+            EmployeeWage Employee2 = new EmployeeWage();
+            Employee2.CalEmployeeWage();
         }
     }
 }
