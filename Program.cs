801c6e9adccea224de895906e469d84e1be01970
murgendrag <murgendra67@gmail.com>
2022-03-07 17:28:57 +0530

    object creation

--- Program.cs
+++ Program.cs
@@ -8,9 +8,25 @@ namespace Day8_PracticeProblems
         //    EmployeeAttendance Employee1 = new EmployeeAttendance();
         //    Employee.Employeeattcheck();
         //}
+        //{
+        //    EmployeeWage Employee2 = new EmployeeWage();
+        //    Employee2.CalEmployeeWage();
+        //}
+        //{
+        //    EmployeeParttime Employee3 = new EmployeeParttime();
+        //    Employee3.CalEmployeewage();
+        //}
+        //{
+        //    WageSwitchcase Employee4 = new WageSwitchcase();
+        //    Employee4.useswitchcase();
+        //}
+        //{
+        //    MonthlyWage Employee5 = new MonthlyWage();
+        //    Employee5.calmonthlywage();
+        //}
         {
-            EmployeeWage Employee2 = new EmployeeWage();
-            Employee2.CalEmployeeWage();
+            Employeemonthly100hours Employee6 = new Employeemonthly100hours();
+            Employee6.calmonthlywage();
         }
     }
 }
